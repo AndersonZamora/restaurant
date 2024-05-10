5a30b2f822411222f1182888b3a9e5ba9ff3f0ec
CinCoutTechnology <cincout.technology@gmail.com>
2024-05-09 22:07:56 -0500

    delete seed-dat

--- database/index.ts
+++ database/index.ts
@@ -2,4 +2,4 @@ export * as db from './db';
 export * as dbUsers from './dbUsers';
 
 // Base de datos de prueba
-export * as seedDatabase from './seed-data';
+// export * as seedDatabase from './seed-data';
